99aa932b3f9977bc3ce1cf593c7890499bb6d8b5
Mateus Junges <contato@mateusjunges.com>
2019-09-28 13:36:20 -0300

    Update migrations to use the config table names

--- resources/database/migrations/0000_00_00_000000_create_taggable_table.php
+++ resources/database/migrations/0000_00_00_000000_create_taggable_table.php
@@ -17,8 +17,10 @@ class CreateTaggableTable extends Migration
     {
         $connection = config('taggable.connection');
 
-        if (!Schema::connection($connection)->hasTable('taggable_tags')) {
-            Schema::connection($connection)->create('taggable_tags', function(Blueprint $table) {
+        $taggable_tags = config('taggable.tables.taggable_tags') ?? 'taggable_tags';
+
+        if (!Schema::connection($connection)->hasTable($taggable_tags)) {
+            Schema::connection($connection)->create($taggable_tags, function(Blueprint $table) {
                 $table->bigIncrements('tag_id');
                 $table->string('name');
                 $table->string('normalized');
@@ -28,8 +30,10 @@ class CreateTaggableTable extends Migration
             });
         }
 
-        if (!Schema::connection($connection)->hasTable('taggable_taggables')) {
-            Schema::connection($connection)->create('taggable_taggables', function(Blueprint $table) {
+        $taggable_taggables = config('taggable.tables.taggable_taggables') ?? 'taggable_tags';
+
+        if (!Schema::connection($connection)->hasTable($taggable_taggables)) {
+            Schema::connection($connection)->create($taggable_taggables, function(Blueprint $table) {
                 $table->unsignedBigInteger('tag_id');
                 $table->unsignedBigInteger('taggable_id');
                 $table->string('taggable_type');
@@ -49,13 +53,15 @@ class CreateTaggableTable extends Migration
     public function down()
     {
         $connection = config('taggable.connection');
+        $taggable_tags = config('taggable.tables.taggable_tags') ?? 'taggable_tags';
+        $taggable_taggables = config('taggable.tables.taggable_taggables') ?? 'taggable_tags';
 
-        if (Schema::connection($connection)->hasTable('taggable_tags')) {
-            Schema::connection($connection)->drop('taggable_tags');
+        if (Schema::connection($connection)->hasTable($taggable_tags)) {
+            Schema::connection($connection)->drop($taggable_tags);
         }
 
-        if (Schema::connection($connection)->hasTable('taggable_taggables')) {
-            Schema::connection($connection)->drop('taggable_taggables');
+        if (Schema::connection($connection)->hasTable($taggable_taggables)) {
+            Schema::connection($connection)->drop($taggable_taggables);
         }
     }
 }
